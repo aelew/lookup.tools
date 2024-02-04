46f74b6c6a717e21a5fc878baecac1786b5d708c
aelew <118043566+aelew@users.noreply.github.com>
2024-02-03 16:04:41 -0800

    fix eslint type warnings

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -1,13 +1,13 @@
-import * as LabelPrimitive from '@radix-ui/react-label';
+import type * as LabelPrimitive from '@radix-ui/react-label';
 import { Slot } from '@radix-ui/react-slot';
 import * as React from 'react';
 import {
   Controller,
-  ControllerProps,
-  FieldPath,
-  FieldValues,
   FormProvider,
-  useFormContext
+  useFormContext,
+  type ControllerProps,
+  type FieldPath,
+  type FieldValues
 } from 'react-hook-form';
 
 import { Label } from '@/components/ui/label';
