55a440f24c8b822f7d0f31da45d00f530e2e106a
aelew <118043566+aelew@users.noreply.github.com>
2024-02-03 16:33:01 -0800

    ignore eslint during build

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ await import('./src/env.js');
 
 /** @type {import("next").NextConfig} */
 const config = {
+  eslint: {
+    ignoreDuringBuilds: true
+  },
   typescript: {
     ignoreBuildErrors: true
   },
