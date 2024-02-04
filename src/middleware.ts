80400810c9b3fb82ffdbc590ed14386540adde24
aelew <118043566+aelew@users.noreply.github.com>
2024-02-04 01:06:42 -0800

    clean up middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,6 @@
 import { NextResponse, type NextRequest } from 'next/server';
 
 export default async function middleware(req: NextRequest) {
-  const headers: Record<string, string> = {};
-  for (const [key, value] of req.headers.entries()) {
-    headers[key] = value;
-  }
   const ipAddress =
     req.headers.get('cf-connecting-ip') ?? req.headers.get('x-forwarded-for');
   if (ipAddress) {
