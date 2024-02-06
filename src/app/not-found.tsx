99a05b34956d9048ca85e213fe60c4c62cb254e6
aelew <118043566+aelew@users.noreply.github.com>
2024-02-05 22:30:21 -0800

    improve 404 not found message

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,11 +8,12 @@ export default function NotFound() {
     <section className="flex flex-col justify-center space-y-4 py-12 sm:py-24">
       <div className="mx-auto flex max-w-5xl items-center justify-center gap-4">
         <h1 className="text-4xl font-semibold tracking-tighter md:text-5xl">
-          Resource not found
+          404 Not Found
         </h1>
       </div>
-      <h2 className="mx-auto max-w-2xl text-center font-medium tracking-tight text-muted-foreground">
-        Sorry, we couldn&apos;t find the page you were looking for.
+      <h2 className="mx-auto max-w-sm text-center font-medium tracking-tight text-muted-foreground">
+        Uh oh! We couldn&apos;t find the resource you were looking for. Please
+        check your URL and try again.
       </h2>
       <Link className={buttonVariants({ className: 'mx-auto w-fit' })} href="/">
         <ArrowLeftIcon className="mr-2 size-4" /> Return home
