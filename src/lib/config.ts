558efdd0b10d04d6a144962ac473d11c1085bfcc
aelew <118043566+aelew@users.noreply.github.com>
2024-02-07 12:01:48 -0800

    update api base url

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,4 +3,4 @@ import { env } from '@/env';
 export const CACHE_REVALIDATE_SECONDS =
   env.NODE_ENV === 'development' ? 15 : 900;
 
-export const API_BASE_URL = 'https://api.aelew.dev';
+export const API_BASE_URL = 'https://api.lookup.tools';
