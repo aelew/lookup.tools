aa43ad820d8de3dd8a7702cde9374eeeac5fdf3a
aelew <118043566+aelew@users.noreply.github.com>
2024-02-07 15:28:36 -0800

    prevent server header exposure

--- src/server/api/routers/client.ts
+++ src/server/api/routers/client.ts
@@ -2,6 +2,10 @@ import { createTRPCRouter, publicProcedure } from '@/server/api/trpc';
 
 export const clientRouter = createTRPCRouter({
   ip: publicProcedure.query(({ ctx }) => {
-    return { data: ctx.headers.get('x-client-ip') };
+    return {
+      data:
+        ctx.headers.get('cf-connecting-ip') ??
+        ctx.headers.get('x-forwarded-for')
+    };
   })
 });
