cf19063b8d0101ffac9adc8cf9b04adad463d9d9
Andre Lew <me@aelew.com>
2025-04-06 20:58:01 -0700

    styling improvements

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -8,15 +8,16 @@ import { GitHubButton } from './github-button';
 export function Header() {
   return (
     <header className="flex h-14 items-center justify-between border-b">
-      <div className="flex items-center gap-8">
-        <Link className="flex items-center gap-2" href="/">
-          <Logo />
-          <span className="whitespace-nowrap font-semibold tracking-tight">
-            Lookup Tools
-          </span>
-        </Link>
-      </div>
-      <div className="flex items-center gap-4">
+      <Link
+        className="flex items-center gap-2 transition-opacity hover:opacity-80"
+        href="/"
+      >
+        <Logo className="size-5" />
+        <span className="whitespace-nowrap text-lg font-semibold tracking-tight">
+          Lookup Tools
+        </span>
+      </Link>
+      <div className="flex items-center gap-2">
         <Suspense>
           <GitHubButton />
         </Suspense>
