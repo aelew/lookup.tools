8f580f288e93a4824556ee70803df337e70e326f
Andre Lew <me@aelew.com>
2025-12-18 14:23:32 -0800

    add caching

--- backend/routers/v1/resolve.py
+++ backend/routers/v1/resolve.py
@@ -8,7 +8,9 @@ from urllib.parse import unquote
 
 import httpx
 import niquests
+import orjson
 import tldextract
+from aiocache import Cache
 from asyncwhois import aio_rdap, aio_whois
 from email_validator import EmailNotValidError, validate_email
 from holehe.core import get_functions, import_submodules, launch_module
@@ -18,10 +20,18 @@ from robyn.robyn import QueryParams
 from urllib3 import Retry
 
 from exceptions import HTTPException
+from utils.cache import CacheDataSerializer
 from utils.dns import CloudflareDNSResolver
 from utils.net import is_cloudflare_ip, ping
 
 router = SubRouter(__file__, prefix="/v1/resolve")
+
+cache = Cache(
+    Cache.MEMORY,
+    serializer=CacheDataSerializer(),
+    namespace="main",
+    ttl=60 * 15,  # 15m
+)
 
 
 class QueryRequestParams(QueryParams):
@@ -59,10 +69,17 @@ async def resolve_dns(query_params: QueryRequestParams):
     if root_domain == "":
         raise HTTPException(status_codes.HTTP_400_BAD_REQUEST, "Domain is invalid")
 
-    resolver = CloudflareDNSResolver()
-    records = await resolver.resolve_records(root_domain)
+    key = f"dns:{root_domain}"
+    response = await cache.get(key)
 
-    return {"q": root_domain, "data": records}
+    if not response:
+        resolver = CloudflareDNSResolver()
+        records = await resolver.resolve_records(root_domain)
+
+        response = {"q": root_domain, "data": records}
+        await cache.set(key, response)
+
+    return response
 
 
 @router.get("/whois")
@@ -78,41 +95,50 @@ async def resolve_whois(query_params: QueryRequestParams):
     if root_domain == "":
         raise HTTPException(status_codes.HTTP_400_BAD_REQUEST, "Domain is invalid")
 
-    normalized_output = None
+    key = f"whois:{root_domain}"
+    response = await cache.get(key)
 
-    # resolve with WHOIS
-    try:
-        raw_output, normalized_output = await aio_whois(root_domain)
-    except Exception:
-        pass
+    if not response:
+        normalized_output = None
 
-    # fallback to RDAP
-    if normalized_output is None or normalized_output["domain_name"] is None:
+        # resolve with WHOIS
         try:
-            raw_output, normalized_output = await aio_rdap(root_domain)
-        except Exception as e:
-            logger.error(f"Failed to resolve WHOIS and RDAP for {root_domain}", e)
+            raw_output, normalized_output = await aio_whois(root_domain)
+        except Exception:
+            pass
 
-    if normalized_output is None:
-        raise HTTPException(
-            status_codes.HTTP_500_INTERNAL_SERVER_ERROR,
-            f"Failed to resolve {root_domain}",
+        # fallback to RDAP
+        if normalized_output is None or normalized_output["domain_name"] is None:
+            try:
+                raw_output, normalized_output = await aio_rdap(root_domain)
+            except Exception as e:
+                logger.error(f"Failed to resolve WHOIS and RDAP for {root_domain}", e)
+
+        if normalized_output is None:
+            raise HTTPException(
+                status_codes.HTTP_500_INTERNAL_SERVER_ERROR,
+                f"Failed to resolve {root_domain}",
+            )
+
+        del normalized_output["domain_name"]
+
+        normalized_output = orjson.loads(
+            orjson.dumps(
+                normalized_output,
+                default=str,
+                option=orjson.OPT_NON_STR_KEYS,
+            )
         )
 
-    del normalized_output["domain_name"]
+        response = {
+            "q": root_domain,
+            "data": normalized_output,
+            "raw": raw_output.strip(),
+        }
 
-    return Response(
-        status_code=status_codes.HTTP_200_OK,
-        description=json.dumps(
-            {
-                "q": root_domain,
-                "data": normalized_output,
-                "raw": raw_output.strip(),
-            },
-            default=str,
-        ),
-        headers={"Content-Type": "application/json"},
-    )
+        await cache.set(key, response)
+
+    return response
 
 
 @router.get("/subdomains")
@@ -128,54 +154,61 @@ async def resolve_subdomains(query_params: QueryRequestParams):
     if root_domain == "":
         raise HTTPException(status_codes.HTTP_400_BAD_REQUEST, "Domain is invalid")
 
-    response = await niquests.aget(
-        "https://crt.sh",
-        params={"q": f"%.{root_domain}", "output": "json"},
-        retries=Retry(total=5, status_forcelist=[502]),
-        timeout=30,
-    )
-    if not response.ok:
-        raise HTTPException(
-            status_codes.HTTP_500_INTERNAL_SERVER_ERROR,
-            f"Third-party lookup failed (status_code={response.status_code})",
+    key = f"subdomains:{root_domain}"
+    response = await cache.get(key)
+
+    if not response:
+        cert_res = await niquests.aget(
+            "https://crt.sh",
+            params={"q": f"%.{root_domain}", "output": "json"},
+            retries=Retry(total=5, status_forcelist=[502]),
+            timeout=30,
         )
+        if not cert_res.ok:
+            raise HTTPException(
+                status_codes.HTTP_500_INTERNAL_SERVER_ERROR,
+                f"Third-party lookup failed (status_code={cert_res.status_code})",
+            )
 
-    certs = response.json()
+        certs = cert_res.json()
 
-    # extract common names and dedupe
-    common_names = set(c.get("common_name") for c in certs if c.get("common_name"))
+        # extract common names and dedupe
+        common_names = set(c.get("common_name") for c in certs if c.get("common_name"))
 
-    # remove wildcard domains and other domains that don't match the input domain
-    common_names = [
-        cn
-        for cn in common_names
-        if "*" not in cn and (cn == root_domain or cn.endswith(f".{root_domain}"))
-    ]
+        # remove wildcard domains and other domains that don't match the input domain
+        common_names = [
+            cn
+            for cn in common_names
+            if "*" not in cn and (cn == root_domain or cn.endswith(f".{root_domain}"))
+        ]
 
-    def fdqn_sort(fqdn: str):
-        # root first
-        if fqdn == root_domain:
-            return (0, fqdn)
+        def fdqn_sort(fqdn: str):
+            # root first
+            if fqdn == root_domain:
+                return (0, fqdn)
 
-        # WWW second
-        if fqdn == f"www.{root_domain}":
-            return (1, fqdn)
+            # WWW second
+            if fqdn == f"www.{root_domain}":
+                return (1, fqdn)
 
-        # everything else alphabetically
-        return (2, fqdn)
+            # everything else alphabetically
+            return (2, fqdn)
 
-    common_names.sort(key=fdqn_sort)
+        common_names.sort(key=fdqn_sort)
 
-    ping_tasks = [ping(cn) for cn in common_names]
-    live_ips = await asyncio.gather(*ping_tasks)
+        ping_tasks = [ping(cn) for cn in common_names]
+        live_ips = await asyncio.gather(*ping_tasks)
 
-    data = [
-        {"fqdn": cn, "ip": ip, "attributes": {"cloudflare": is_cloudflare_ip(ip)}}
-        for cn, ip in zip(common_names, live_ips)
-        if ip
-    ]
+        data = [
+            {"fqdn": cn, "ip": ip, "attributes": {"cloudflare": is_cloudflare_ip(ip)}}
+            for cn, ip in zip(common_names, live_ips)
+            if ip
+        ]
 
-    return {"q": root_domain, "data": data}
+        response = {"q": root_domain, "data": data}
+        await cache.set(key, response, ttl=60 * 30)  # 30m
+
+    return response
 
 
 @router.get("/ip")
@@ -190,26 +223,33 @@ async def resolve_ip(query_params: QueryRequestParams):
     except ValueError:
         raise HTTPException(status_codes.HTTP_400_BAD_REQUEST, "Address is invalid")
 
-    res = await niquests.aget(
-        f"https://api.ipdata.co/v1/{query}",
-        params={"api-key": os.getenv("IPDATA_API_KEY")},
-    )
+    key = f"ip:{query}"
+    response = await cache.get(key)
 
-    if not res.ok:
-        raise HTTPException(
-            status_codes.HTTP_500_INTERNAL_SERVER_ERROR,
-            f"Third-party lookup failed (status_code={res.status_code})",
+    if not response:
+        res = await niquests.aget(
+            f"https://api.ipdata.co/v1/{query}",
+            params={"api-key": os.getenv("IPDATA_API_KEY")},
         )
 
-    data = res.json()
+        if not res.ok:
+            raise HTTPException(
+                status_codes.HTTP_500_INTERNAL_SERVER_ERROR,
+                f"Third-party lookup failed (status_code={res.status_code})",
+            )
 
-    try:
-        del data["ip"]
-        del data["count"]
-    except KeyError:
-        pass
+        data = res.json()
 
-    return {"q": query, "data": data}
+        try:
+            del data["ip"]
+            del data["count"]
+        except KeyError:
+            pass
+
+        response = {"q": query, "data": data}
+        await cache.set(key, response, ttl=60 * 60 * 24 * 3)  # 3d
+
+    return response
 
 
 @router.get("/accounts")
@@ -229,18 +269,25 @@ async def resolve_accounts(query_params: QueryRequestParams):
             status_codes.HTTP_400_BAD_REQUEST, f"Invalid email address — {str(e)}"
         )
 
-    modules = import_submodules("holehe.modules")
-    websites = get_functions(modules)
+    key = f"accounts:{query}"
+    response = await cache.get(key)
 
-    client = httpx.AsyncClient(timeout=10)
-    out = []
+    if not response:
+        modules = import_submodules("holehe.modules")
+        websites = get_functions(modules)
 
-    await asyncio.gather(*[launch_module(w, query, client, out) for w in websites])
-    await client.aclose()
+        client = httpx.AsyncClient(timeout=10)
+        out = []
 
-    data = {}
-    for w in sorted(out, key=lambda w: w["name"]):
-        if not w["rateLimit"]:
-            data[w["domain"]] = w["exists"]
+        await asyncio.gather(*[launch_module(w, query, client, out) for w in websites])
+        await client.aclose()
 
-    return {"q": query, "data": data}
+        data = {}
+        for w in sorted(out, key=lambda w: w["name"]):
+            if not w["rateLimit"]:
+                data[w["domain"]] = w["exists"]
+
+        response = {"q": query, "data": data}
+        await cache.set(key, response, ttl=60 * 60)  # 1h
+
+    return response
