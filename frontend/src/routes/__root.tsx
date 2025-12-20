2ef7cd7912e880dc4402d6c887f4dc8bea7629e7
Andre Lew <me@aelew.com>
2025-12-19 17:19:18 -0800

    favicons

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -28,6 +28,34 @@ export const Route = createRootRouteWithContext<RootRouteContext>()({
       {
         rel: 'stylesheet',
         href: styles
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: '/favicon-96x96.png',
+        sizes: '96x96'
+      },
+      {
+        rel: 'icon',
+        type: 'image/svg+xml',
+        href: '/favicon.svg'
+      },
+      {
+        rel: 'shortcut icon',
+        href: '/favicon.ico'
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png'
+      },
+      {
+        name: 'apple-mobile-web-app-title',
+        content: 'Lookup Tools'
+      },
+      {
+        rel: 'manifest',
+        href: '/site.webmanifest'
       }
     ]
   })
