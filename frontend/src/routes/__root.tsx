4d72c08175a8533e66d9e52d8d1627730d55d6ce
Andre Lew <me@aelew.com>
2026-05-05 18:04:14 -0700

    add Plausible Analytics script

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -69,6 +69,19 @@ function RootDocument({ children }: PropsWithChildren) {
     <html lang="en" suppressHydrationWarning={import.meta.env.DEV}>
       <head>
         <HeadContent />
+        {import.meta.env.VITE_MAIN_INSTANCE === '1' && (
+          <>
+            <script
+              src="https://analytics.aelew.dev/js/pa-riSECZHCgWmjtqo3VRd0P.js"
+              async
+            />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};plausible.init()`
+              }}
+            />
+          </>
+        )}
       </head>
       <body className="relative flex min-h-screen flex-col">
         <Providers queryClient={queryClient}>
